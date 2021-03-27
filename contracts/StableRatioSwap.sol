f9f197b4343d46268008ae8b86e8da1baa4f8f29
Hideaki Yoshida <hyoshida@berkeley.edu>
2021-03-28 04:21:09 +0900

    Changed the compiler version in hardhat.config

--- contracts/StableRatioSwap.sol
+++ contracts/StableRatioSwap.sol
@@ -1,9 +1,10 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+pragma solidity >=0.6.12;
 pragma experimental ABIEncoderV2;
 
 import "hardhat/console.sol";
 import {ILendingPool} from "@aave/protocol-v2/contracts/interfaces/ILendingPool.sol";
+import {ILendingPoolAddressesProvider} from '@aave/protocol-v2/contracts/interfaces/ILendingPoolAddressesProvider.sol';
 
 contract StableRatioSwap {
 
